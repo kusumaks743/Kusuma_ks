facd4aef3c2fd8fc0d4a107e788502c7473af224
kusumaks743 <kusumaks743@gmail.com>
2024-11-25 23:37:32 -0800

    Add files via upload

---
+++ main age.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+  int age = 20;
+  printf("enter the number") scanf("%d", &age);
+  if (age > 18) {
+    if (age < 30) {
+      printf("you are a young adult");
+    }
+  }
