abdcd2ac9a780763777692b1dece288127482efa
kusumaks743 <kusumaks743@gmail.com>
2024-11-27 16:26:20 +0530

    Create Largestvalues.C

---
+++ Largestvalues.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int large(int a,int b,int c)
+{
+  if(a>=b && a>=c)return a;
+  if(b>=a && b>=c) return b;
+  return c;
+}
+int main() {
+  int x,y,z;
+  printf("enter three numbers");
+  scanf("%d%d%d",&x,&y,&z);
+  printf("largest=%d\n",large(x,y,z));
+  return 0;
+}
