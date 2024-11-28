d3dcfa9f3a91c9a71a04c04b060615a36ea27e2e
kusumaks743 <kusumaks743@gmail.com>
2024-11-28 16:22:45 +0530

    Create Dynamic Memory.C

---
+++ Dynamic Memory.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+  int n,*arr;
+  printf("Enter the number of elements:");
+  scanf("%d",&n);
+  arr=(int*)malloc(n*sizeof(int));
+  if(arr==NULL){
+    printf("Memory allocation failed\n");
+    return 1;
+  }
+  printf("Enter%d elements:\n",n);
+  for(int i=0;i<n;i++){
+    scanf("%d",&arr[i]);
+  }
+  printf("The elements are:\n");
+  for(int i=0;i<n;i++){
+    printf("%d",arr[i]);
+}
+free(arr);
+return 0;
+}
